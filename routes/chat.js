5dc91aef2a15dc08623056f016739a5bbbe12a60
andres malaisse <malaisse_andres@sfr.fr>
2022-12-12 11:58:28 +0100

    chat backend

---
+++ routes/chat.js
@@ -0,0 +1,37 @@
+var express = require("express");
+var router = express.Router();
+
+const Pusher = require("pusher");
+const pusher = new Pusher({
+  appId: process.env.PUSHER_APPID,
+  key: process.env.PUSHER_KEY,
+  secret: process.env.PUSHER_SECRET,
+  cluster: process.env.PUSHER_CLUSTER,
+  useTLS: true,
+});
+
+router.put("/users/:firstname", (req, res) => {
+  pusher.trigger("chat", "join", {
+    firstname: req.params.firstname,
+  });
+
+  res.json({ result: true });
+});
+
+router.delete("/users/:firstname", (req, res) => {
+  pusher.trigger("chat", "leave", {
+    firstname: req.params.firstname,
+  });
+
+  res.json({ result: true });
+});
+
+router.post("/message", (req, res) => {
+  const message = req.body;
+
+  pusher.trigger("chat", "message", message);
+
+  res.json({ result: true });
+});
+
+module.exports = router;
