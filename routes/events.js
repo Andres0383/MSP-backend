4c69ffdcbbf09a723bc568d88ed508ca4eb13f14
andres malaisse <malaisse_andres@sfr.fr>
2022-12-19 10:21:13 +0100

    test

--- routes/events.js
+++ routes/events.js
@@ -27,7 +27,7 @@ const Event = require("../models/events");
 //get all avents
 router.get("/all", (req, res) => {
   Event.find()
-    .populate("user")
+    .populate("user", ["firstname", "level"])
 
     .then((events) => {
       res.json({ result: true, events });
