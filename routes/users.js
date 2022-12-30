7edd972915c12ceba70f995d9e5c230cd5053a5f
andres malaisse <malaisse_andres@sfr.fr>
2022-12-31 00:34:17 +0100

    get date

--- routes/users.js
+++ routes/users.js
@@ -114,7 +114,7 @@ router.put("/update/", (req, res) => {
     {
       sport,
       level,
-      dateOfBirth: new Date(dateOfBirth),
+      dateOfBirth: new Date(dateOfBirth.getDate() + 1),
       sex,
       mixedSex,
       city,
